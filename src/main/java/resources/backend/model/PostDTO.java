f07452441f418a58d0d601b4a414214dcda9cf6f
XoforickS <alexandre77.nourry@hotmail.fr>
2024-02-06 14:58:38 +0100

    Posts | Get, Delete, Update, Create working

---
+++ src/main/java/resources/backend/model/PostDTO.java
@@ -0,0 +1,77 @@
+package resources.backend.model;
+
+import jakarta.validation.constraints.Size;
+
+public class PostDTO {
+
+    private Long id;
+
+    @Size(max = 120)
+    private String title;
+
+    private String content;
+
+    private String publicationDate;
+
+    private Boolean popular;
+
+    private Long userId;
+
+    private Long categoryId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Boolean getPopular() {
+        return popular;
+    }
+
+    public void setPopular(Boolean popular) {
+        this.popular = popular;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
--- src/main/java/resources/backend/repos/CustomerRepository.java
+++ src/main/java/resources/backend/repos/CustomerRepository.java
@@ -3,8 +3,6 @@ package resources.backend.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import resources.backend.domain.Customer;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmail(String email);
 }
