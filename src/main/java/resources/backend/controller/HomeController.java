0ec66ab7b300a94c0cbe282d17bd1ea501d1ff3e
Martin Durand <104625791+MrPatapooka@users.noreply.github.com>
2024-03-07 09:26:01 +0100

    Coquilles sonarCube

--- src/main/java/resources/backend/controller/HomeController.java
+++ src/main/java/resources/backend/controller/HomeController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     @ResponseBody
     public String index() {
-        return "Hello World!";
+        return System.getenv("LOGIN");
     }
-
 }
