7ea3d6fc9883047dd5dbfb897b93378d06c077e0
Thibault <muelthibault@gmail.com>
2024-04-10 08:13:09 +0200

    comment customerModel

--- src/main/java/resources/backend/model/CustomerModel.java
+++ src/main/java/resources/backend/model/CustomerModel.java
@@ -1,71 +1,71 @@
-package resources.backend.model;
+// package resources.backend.model;
 
-import jakarta.validation.constraints.Size;
+// import jakarta.validation.constraints.Size;
 
-public class CustomerModel {
+// public class CustomerModel {
 
-    private Long id;
+//     private Long id;
 
-    @Size(max = 255)
-    private String name;
+//     @Size(max = 255)
+//     private String name;
 
-    @Size(max = 255)
-    private String username;
+//     @Size(max = 255)
+//     private String username;
 
-    @Size(max = 255)
-    private String email;
+//     @Size(max = 255)
+//     private String email;
 
-    @Size(max = 255)
-    private String password;
+//     @Size(max = 255)
+//     private String password;
 
-    @Size(max = 255)
-    private String role;
+//     @Size(max = 255)
+//     private String role;
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
+//     public void setId(final Long id) {
+//         this.id = id;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+//     public void setName(final String name) {
+//         this.name = name;
+//     }
 
-    public String getUsername() {
-        return username;
-    }
+//     public String getUsername() {
+//         return username;
+//     }
 
-    public void setUsername(final String username) {
-        this.username = username;
-    }
+//     public void setUsername(final String username) {
+//         this.username = username;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(final String email) {
-        this.email = email;
-    }
+//     public void setEmail(final String email) {
+//         this.email = email;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(final String password) {
-        this.password = password;
-    }
+//     public void setPassword(final String password) {
+//         this.password = password;
+//     }
 
-    public String getRole() {
-        return role;
-    }
+//     public String getRole() {
+//         return role;
+//     }
 
-    public void setRole(final String role) {
-        this.role = role;
-    }
-}
+//     public void setRole(final String role) {
+//         this.role = role;
+//     }
+// }
